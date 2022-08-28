a04dc60fb736fad6dfda9ff635f8bbbaf08e6e68
nessaf khan <nessafkhan.kn@gmail.com>
2022-08-28 23:29:15 +0530

    api integrated

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = 'https://wysa-backend.herokuapp.com/api';
 
 export const API_REQUEST_URL = axios.create({
 	baseURL: BASE_URL,
