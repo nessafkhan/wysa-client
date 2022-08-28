e0aec998fe633348b2601627ab3a3f2bf49adcb9
nessaf khan <nessafkhan.kn@gmail.com>
2022-08-28 23:46:31 +0530

    fixed production bug

--- src/Screens/Screen_login.js
+++ src/Screens/Screen_login.js
@@ -4,7 +4,6 @@ import DownArrowButton from '../Components/DownArrowButoon/DownArrowButton';
 import ScreenStyles from './Screen.module.css';
 import { useNavigate } from 'react-router-dom';
 import { API_REQUEST_URL } from '../config/axios.config';
-import { yellow } from '@mui/material/colors';
 
 const ScreenLogin = () => {
 	const [nickname, setNickname] = useState('');
