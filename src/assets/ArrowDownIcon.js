0e26f1fc35cbe2cfe3d2c958905b5f375fbee533
nessaf khan <nessafkhan.kn@gmail.com>
2022-08-29 13:03:28 +0530

    style changed

--- src/assets/ArrowDownIcon.js
+++ src/assets/ArrowDownIcon.js
@@ -4,8 +4,8 @@ const ArrowDownIcon = () => {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			height="28"
-			width="28"
+			height="currentHeight"
+			width="currentWidth"
 			fill="currentColor"
 		>
 			<path d="M24 40 8 24l2.1-2.1 12.4 12.4V8h3v26.3l12.4-12.4L40 24Z" />
